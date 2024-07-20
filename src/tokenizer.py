f49ce8d1e7a1f96c7f125c262408eebfbe3495cf
Dhruv Sharma <dhruv.sharma@polytechnique.org>
2024-07-19 22:58:07 -0400

    Handle unknown characters within encoding function: https://github.com/dhruvshrma/gpt2-in-mlx/issues/1

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -5,12 +5,15 @@ def prepare_vocab(input_text: str) -> Tuple[Dict[str, int], Dict[int, str]]:
     vocab = sorted(list(set(input_text)))
     ## Why is sorting important here?
     vocab_to_int = {c: i for i, c in enumerate(vocab)}
-    int_to_vocab = {i: c for c, i in vocab_to_int.items()}
+    int_to_vocab = {i: c for i, c in enumerate(vocab)}
     return vocab_to_int, int_to_vocab
 
 
 def encode_text(input_string: str, string_to_int: Dict[str, int]) -> List[int]:
-    return [string_to_int[c] for c in input_string]
+    try:
+        return [string_to_int[c] for c in input_string]
+    except KeyError:
+        raise KeyError("Unknown token in input string")
 
 
 def decode_text(encoded_string: List[int], int_to_string: Dict[int, str]) -> str:
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.tokenizer import *
 import os
 
@@ -5,18 +7,29 @@ base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 data_path = os.path.join(base_path, "data")
 
 
-def test_create_vocab() -> None:
+@pytest.fixture
+def input_text() -> str:
     file_name = os.path.join(data_path, "data.txt")
     with open(file_name, "r") as f:
-        input_text = f.read()
+        return f.read()
+
+
+def test_create_vocab(input_text) -> None:
     vocab_to_int, int_to_vocab = prepare_vocab(input_text)
     assert len(vocab_to_int) == len(int_to_vocab)
     assert len(vocab_to_int) == 65
 
 
-def test_encode_decode() -> None:
-    input_text: str = "Hello World!"
+def test_encode_decode(input_text) -> None:
     vocab_to_int, int_to_vocab = prepare_vocab(input_text)
-    encoded_text = encode_text(input_text, vocab_to_int)
+    test_text = "Hello, World!"
+    encoded_text = encode_text(test_text, vocab_to_int)
     decoded_text = decode_text(encoded_text, int_to_vocab)
-    assert input_text == decoded_text
+    assert test_text == decoded_text
+
+
+def test_unknown_tokens_throws_error(input_text) -> None:
+    vocab_to_int, int_to_vocab = prepare_vocab(input_text)
+    test_text = "Hello World`"
+    with pytest.raises(KeyError):
+        encode_text(test_text, vocab_to_int)
